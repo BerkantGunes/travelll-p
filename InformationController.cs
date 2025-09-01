bbf8cc442fce186a4178529292bb0a427c93c347
Berkant Gunes <gunesberkant96@gmail.com>
2025-09-01 12:37:51 +0300

    Add files via upload

---
+++ InformationController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Traveler.Areas.Member.Controllers
+{
+    [Area("Member")]
+    public class InformationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
