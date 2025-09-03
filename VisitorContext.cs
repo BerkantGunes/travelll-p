4d0eeeb98b84d734d133f57115f3bd12f9e55fbc
Berkant Gunes <gunesberkant96@gmail.com>
2025-09-03 18:12:36 +0300

    Add files via upload

---
+++ VisitorContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TravelerApi.DAL.Entities;
+
+namespace TravelerApi.DAL.Context
+{
+    public class VisitorContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=GUNESBERKANT\\SQLEXPRESS;initial catalog=TravelerDbApi;integrated security=true;encrypt=false;");
+        }
+        public DbSet<Visitor> Visitors { get; set; }
+    }
+}
