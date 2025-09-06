3377640c7008e2a2f1239f03f42bec2122dc6ab6
Berkant Gunes <gunesberkant96@gmail.com>
2025-09-06 19:49:22 +0300

    Add files via upload

---
+++ CreateDestinationCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace Traveler.CQRS.Commands.DestinationCommand
+{
+    public class CreateDestinationCommand
+    {
+        public string City { get; set; }
+        public string DayNight { get; set; }
+        public double Price { get; set; }
+        public int Capacity { get; set; }
+        public bool Status { get; set; }
+    }
+}
