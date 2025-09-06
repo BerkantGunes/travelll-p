af75b482ec891cab15e56a2ed079ccb8a5266074
Berkant Gunes <gunesberkant96@gmail.com>
2025-09-06 19:50:28 +0300

    Add files via upload

---
+++ GetAllDestinationQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Traveler.CQRS.Results.DestinationResult;
+
+namespace Traveler.CQRS.Handlers.DestinationHandler
+{
+    public class GetAllDestinationQueryHandler
+    {
+        private readonly Context _context;
+
+        public GetAllDestinationQueryHandler(Context context)
+        {
+            _context = context;
+        }
+
+        public List<GetAllDestinationQueryResult> Handle()
+        {
+            var values = _context.Destinations.Select(x => new GetAllDestinationQueryResult
+            {
+                id = x.DestinationId,
+                capacity = x.Capacity,
+                city = x.City,
+                daynight = x.DayNight,
+                price = x.Price
+            }).AsNoTracking().ToList();
+            return values;
+        }
+    }
+}
