b3dc30cb4a7a9c02787fa527dc9b6ded03f79730
Berkant Gunes <gunesberkant96@gmail.com>
2025-09-06 19:49:34 +0300

    Add files via upload

---
+++ RemoveDestinationCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace Traveler.CQRS.Commands.DestinationCommand
+{
+    public class RemoveDestinationCommand
+    {
+        public RemoveDestinationCommand(int ıd)
+        {
+            Id = ıd;
+        }
+
+        public int Id { get; set; }
+    }
+}
