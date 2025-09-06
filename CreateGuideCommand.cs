00c77faeb4aaa8be423f4401e139c1113651ba1a
Berkant Gunes <gunesberkant96@gmail.com>
2025-09-06 19:50:03 +0300

    Add files via upload

---
+++ CreateGuideCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Traveler.CQRS.Commands.GuideCommand
+{
+    public class CreateGuideCommand:IRequest
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+}
