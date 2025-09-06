5287ad76ecc32191150ad3d92be6e7f9c839fe0e
Berkant Gunes <gunesberkant96@gmail.com>
2025-09-06 19:49:48 +0300

    Add files via upload

---
+++ UpdateDestinationCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace Traveler.CQRS.Commands.DestinationCommand
+{
+    public class UpdateDestinationCommand
+    {
+        public int DestinationId { get; set; }
+        public string City { get; set; }
+        public string DayNight { get; set; }
+        public double Price { get; set; }
+    }
+}
