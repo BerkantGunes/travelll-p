12f61e3feb0740468b3a2b886792dc3aae8e583d
Berkant Gunes <gunesberkant96@gmail.com>
2025-09-07 06:56:31 +0300

    Add files via upload

---
+++ GetGuideByIdQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using DataAccessLayer.Concrete;
+using MediatR;
+using NuGet.Protocol.Plugins;
+using Traveler.CQRS.Queries.GuideQuery;
+using Traveler.CQRS.Results.GuideResult;
+
+namespace Traveler.CQRS.Handlers.GuideHandler
+{
+    public class GetGuideByIdQueryHandler : IRequestHandler<GetGuideByIdQuery, GetGuideByIdQueryResult>
+    {
+        private readonly Context _context;
+
+        public GetGuideByIdQueryHandler(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<GetGuideByIdQueryResult> Handle(GetGuideByIdQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _context.Guides.FindAsync(request.Id);
+            return new GetGuideByIdQueryResult
+            {
+                GuideId = values.GuideId,
+                Description = values.Description,
+                Name = values.Name
+            };
+        }
+    }
+}
