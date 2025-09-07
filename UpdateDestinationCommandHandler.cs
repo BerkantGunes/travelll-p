de1a2fe4e5f9fd748cbc13982efc0c8e45a0e53e
Berkant Gunes <gunesberkant96@gmail.com>
2025-09-07 06:55:54 +0300

    Add files via upload

---
+++ UpdateDestinationCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using DataAccessLayer.Concrete;
+using Traveler.CQRS.Commands.DestinationCommand;
+
+namespace Traveler.CQRS.Handlers.DestinationHandler
+{
+    public class UpdateDestinationCommandHandler
+    {
+        private readonly Context _context;
+
+        public UpdateDestinationCommandHandler(Context context)
+        {
+            _context = context;
+        }
+        public void Handle(UpdateDestinationCommand command)
+        {
+            var values = _context.Destinations.Find(command.DestinationId);
+            values.City = command.City;
+            values.DayNight = command.DayNight;
+            values.Price = command.Price;
+            _context.SaveChanges();
+        }
+    }
+}
