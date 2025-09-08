3be6c3c8a5b5f67c06ec754c6244e8ba92d7964b
Berkant Gunes <gunesberkant96@gmail.com>
2025-09-08 08:54:37 +0300

    Add files via upload

---
+++ GetDestinationByIdQuery.cs
@@ -0,0 +1,12 @@
+﻿namespace Traveler.CQRS.Queries.DestinationQuery
+{
+    public class GetDestinationByIdQuery
+    {
+        public GetDestinationByIdQuery(int id)
+        {
+            this.id = id;
+        }
+
+        public int id { get; set; }
+    }
+}
