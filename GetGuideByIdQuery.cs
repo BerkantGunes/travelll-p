b88f8e2336ab0d677790e2ab21d109a50b66378e
Berkant Gunes <gunesberkant96@gmail.com>
2025-09-08 08:55:03 +0300

    Add files via upload

---
+++ GetGuideByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Traveler.CQRS.Results.GuideResult;
+
+namespace Traveler.CQRS.Queries.GuideQuery
+{
+    public class GetGuideByIdQuery:IRequest<GetGuideByIdQueryResult>
+    {
+        public GetGuideByIdQuery(int ıd)
+        {
+            Id = ıd;
+        }
+
+        public int Id { get; set; }
+    }
+}
