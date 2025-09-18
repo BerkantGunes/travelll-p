c9e28ea0cc8d9160bb5a091a4ecfed1aa262a48a
Berkant Gunes <gunesberkant96@gmail.com>
2025-09-18 18:35:09 +0300

    Add files via upload

---
+++ SendContactUsValidator.cs
@@ -0,0 +1,25 @@
+﻿using DTOLayer.DTOs.ContactDTOs;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class SendContactUsValidator:AbstractValidator<SendMessageDto>
+    {
+        public SendContactUsValidator()
+        {
+            RuleFor(x => x.Mail).NotEmpty().WithMessage("Mail field can not be null!");
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name field can not be null!");
+            RuleFor(x => x.Subject).NotEmpty().WithMessage("Subject field can not be null!");
+            RuleFor(x => x.MessageBody).NotEmpty().WithMessage("Message field can not be null!");
+            RuleFor(x => x.Subject).MinimumLength(5).WithMessage("Subject must be at least 5 characters!");
+            RuleFor(x => x.Subject).MinimumLength(150).WithMessage("Subject must be maximum 150 characters!");
+            RuleFor(x => x.Mail).MinimumLength(5).WithMessage("Mail must be at least 5 characters!");
+            RuleFor(x => x.Mail).MinimumLength(150).WithMessage("Mail must be maximum 150 characters!");
+        }
+    }
+}
